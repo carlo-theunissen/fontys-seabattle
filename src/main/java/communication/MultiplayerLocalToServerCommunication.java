0fec9cc1b1a0838f50467eb49d0003737cf6d77f
AlextheGreat1509 <Alexfacebook1509@gmail.com>
2018-04-18 08:56:04 +0200

    final commit

--- src/main/java/communication/MultiplayerLocalToServerCommunication.java
+++ src/main/java/communication/MultiplayerLocalToServerCommunication.java
@@ -14,7 +14,7 @@ public class MultiplayerLocalToServerCommunication extends BaseCommunication imp
     public MultiplayerLocalToServerCommunication() throws IOException, DeploymentException {
         helper = new MultiplayerSerialHelper();
 
-        URI uri = URI.create("ws://localhost:8080/game/");
+        URI uri = URI.create("ws://145.93.168.195:8080/game/");
         WebSocketContainer container = ContainerProvider.getWebSocketContainer();
         session = container.connectToServer(new EventClientSocket(),uri);
     }
